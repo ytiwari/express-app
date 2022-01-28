772ef1c5666848c9714262e13ade5b3ea25a03c0
Yogesh Tiwari <yogesh.tiwari@intelligaia.com>
2022-01-28 20:40:08 +0530

    updated docker file

--- routes/shop.js
+++ routes/shop.js
@@ -5,6 +5,9 @@ const express = require("express");
 const route = express.Router();
 const rootDir = require("../util/path");
 
+route.get("/download", (req, res, next) =>{
+    res.download('public/download.jfif');
+});
 route.get("/", (req, res, next) =>{
     res.render(path.join(rootDir, 'views', 'shop'), {title:'Shop'});
 });
